47d6b96fc3149e166b3f776b4cae06c4699a677f
JimmyMcBride <mcbride967@gmail.com>
2020-06-29 21:29:46 -0500

    export as markdown file complete

--- src/views/MarkdownEditor/index.tsx
+++ src/views/MarkdownEditor/index.tsx
@@ -5,8 +5,9 @@ import { useParams } from "react-router";
 import { useQuery, useMutation } from "@apollo/react-hooks";
 import { getFileBySlugQuery, editFileMutation } from "../../graphql";
 import { useSelector } from "react-redux";
-import { Flex, Text, Box, theme } from "sriracha-ui";
+import { Flex, Text, Box, Button, ToolTip, theme } from "sriracha-ui";
 import { storage } from "../../config/firebase";
+import { saveAs } from "file-saver";
 
 export default function MarkdownEditor() {
   const uid = useSelector((state: any) => state.auth?.uid);
@@ -67,6 +68,14 @@ export default function MarkdownEditor() {
       });
   }
 
+  async function exportMarkdownFile(file: any) {
+    await handleSave(localStorage.getItem(slug));
+    const blob = new Blob([file.body], {
+      type: "text/plain;charset=utf-8",
+    });
+    saveAs(blob, `${file.slug}.md`);
+  }
+
   useEffect(() => {
     if (value === "") {
       if (fileData?.getFileBySlug?.body) {
@@ -76,25 +85,35 @@ export default function MarkdownEditor() {
     setCurrTitle(fileData?.getFileBySlug?.title);
   }, [fileData, value]);
 
-  useEffect(() => {
-    if (value === "") {
-      if (fileData?.getFileBySlug?.body) {
-        setValue(fileData?.getFileBySlug?.body);
-      }
-    }
-  }, [fileData, value]);
-
   return (
     <Navbar>
-      <Flex stretch h="4rem" aiCenter>
-        <Box w="2rem" />
-        <Text as="h2" bold lf>
-          {currTitle}
-        </Text>
-        <Box w="2rem" />
-        <Text bold color={theme.colors.red5}>
-          {loading ? "Loading file upload..." : null}
-        </Text>
+      <Flex stretch h="4rem" aiCenter jcBetween>
+        <Flex>
+          <Box w="2rem" />
+          <Text as="h2" bold lf>
+            {currTitle}
+          </Text>
+          <Box w="2rem" />
+          <Text bold color={theme.colors.red5}>
+            {loading ? "Loading file upload..." : null}
+          </Text>
+        </Flex>
+        <Flex>
+          <ToolTip ttRight="4.2rem" ttTop="5.6rem">
+            <Button
+              blue
+              p="0.5rem 1rem"
+              onClick={() => exportMarkdownFile(fileData?.getFileBySlug)}
+            >
+              <Box sqr="1.2rem" bg={theme.colors.lightBlue1} window />
+            </Button>
+            <div className="tooltip">
+              <Text sf color={theme.colors.gray9} w="14rem">
+                Export as markdown file. (Don't forget to save first!)
+              </Text>
+            </div>
+          </ToolTip>
+        </Flex>
       </Flex>
       <div className="demo-container">
         <Editor
