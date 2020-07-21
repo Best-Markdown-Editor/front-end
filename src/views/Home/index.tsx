d6107ddc815b212e9dcff6b59035a9ebfc126c96
JimmyMcBride <mcbride967@gmail.com>
2020-07-21 17:14:08 -0500

    added audiable banner

--- src/views/Home/index.tsx
+++ src/views/Home/index.tsx
@@ -11,6 +11,30 @@ export default function Home() {
           Welcome to Best Markdown Editor!
         </Text>
       </Card>
+      {window.matchMedia("(min-width: 728px)").matches ? (
+        <iframe
+          src="//rcm-na.amazon-adsystem.com/e/cm?o=1&p=48&l=ur1&category=audiblefreetrial&banner=04YSFJHVY8QYH4YVBXG2&f=ifr&linkID={{link_id}}&t=jimmymcbride-20&tracking_id=jimmymcbride-20"
+          scrolling="no"
+          title="desktop banner"
+          style={{
+            border: "none",
+            width: "728px",
+            height: "90px",
+          }}
+        />
+      ) : (
+        <iframe
+          src="//rcm-na.amazon-adsystem.com/e/cm?o=1&p=12&l=ur1&category=audiblefreetrial&banner=0YMF8P30ZEBG4A1N9J82&f=ifr&linkID={{link_id}}&t=jimmymcbride-20&tracking_id=jimmymcbride-20"
+          scrolling="no"
+          title="mobile banner"
+          style={{
+            border: "none",
+            width: "300px",
+            height: "250px",
+          }}
+        />
+      )}
+
       <FileList />
     </Navbar>
   );
